b4ced73e78c01632c7eb75f0bcf090f20626ef66
ishaqmusaq <58223968+ishaqmusaq@users.noreply.github.com>
2020-09-26 01:07:32 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,76 @@
+/*var name = 'ishaq musah'
+
+var school = 'islamic senior high school'
+var addrress = 'madina_zongo'
+var age = 12
+var a = 9;
+var b = 10;
+var c = 50;
+var d = 20;
+
+const e = 20;
+const f = 11;
+const ama = 'ali ibra';
+  console.log(ama);
+  console.log(school);
+  document.write(c/d);
+  document.write(a*b);
+  document.write(school);
+
+
+
+var number = 0;
+while (number<=12)
+{console.log(number);
+number = number+2;}
+
+do{
+  var yourname = prompt("who are u ","Name");
+}
+while(!yourname);
+console.log(yourname);
+
+
+var result = 1;
+var counter = 0;
+while (counter < 10)
+  {
+    result  *= 2; 
+    counter  += 1;
+  }
+console.log(result);
+
+
+for (var current=29;;current++) {
+  if (current % 7==0)
+  break;
+}
+console.log(current);
+
+var person = ["john","ishaq","musah"];
+document.getElementById("demo").innerHTML=person[2];*/
+
+
+
+let dog = {
+  name:"bingo",
+  age:3,
+  locaton:"texas",
+  color: "brown",
+  height:"2ft",
+  siblngs:["hhojw","hkdjj","hkj"],
+  mate:{
+    name:"lola",
+    age:2,
+    color:"grey",
+    locaton:"transasco",
+    height:"3tf"
+  }{
+
+console.log(bingo.mate.name)
+}
+
+function add_names_of_class_list() {
+  let
+}
+
